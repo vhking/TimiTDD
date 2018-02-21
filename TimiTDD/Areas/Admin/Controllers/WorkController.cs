acc5d288f31c31bc8e470715186dacb43149b5ec
vhking <vidarheltne@hotmail.no>
2018-02-21 12:16:02 +0100

    List by year and with total hour worked

--- TimiTDD/Areas/Admin/Controllers/WorkController.cs
+++ TimiTDD/Areas/Admin/Controllers/WorkController.cs
@@ -5,11 +5,15 @@ using TimiTDD.Models.IRepository;
 
 namespace TimiTDD.Areas.Admin.Controllers
 {
+
+    [Area("Admin")]
     public class WorkCotroller : Controller
     {
         private readonly IGenericRepository<WorkParticipation> _workRepository;
         
+        // The ID for "Prosjektarbeid" in ActivityType
         private static int projectWork = 1;
+        // The ID for "Timearbeid" in ActivityType
         private static int hourWork = 2;
         
         public WorkCotroller(IGenericRepository<WorkParticipation> workRepository)
@@ -23,6 +27,12 @@ namespace TimiTDD.Areas.Admin.Controllers
             var workDoneList = _workRepository.GetAll.Where(w=>w.ActivityTypeId == projectWork).Where(w=>w.ActivityTypeId == hourWork);
 
             return View(workDoneList.OrderByDescending(d=>d.DateTimeStart.Value.Year).OrderByDescending(d=>d.DateTimeStart.Value.Date));
+        }
+
+        public IActionResult WorkByYear()
+        {
+            var workYear = _workRepository.GetAll.Select(x=> new {x.DateTimeStart, x.Hours});
+            return View(workYear.OrderByDescending(d=>d.DateTimeStart.Value.Year));
         }
     }
 }
